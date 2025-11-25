41a2dc8b8343ec87f240eae76dc530662c475ea7
ana julia <ana.j2007@aluno.ifsc.edu.br>
2025-11-25 08:53:12 -0300

    corrigido

--- app/src/main/java/com/example/mariaclara/MainActivity.java
+++ app/src/main/java/com/example/mariaclara/MainActivity.java
@@ -1,24 +1,46 @@
 package com.example.mariaclara;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView numero;
+
+    int contador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        numero = findViewById(R.id.text);
+        numero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView numero = (TextView) view;
+                numero.setText("Click here");
+                contador++;
+                numero.setText(Integer.toString(contador));
+
+            }
+        });
+
+        button = findViewById(R.id.botao);
+        button.setText("Click here");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button b = (Button) view;
+                b.setText("Click here");
+                contador++;
+                numero.setText(Integer.toString(contador));
+            }
         });
     }
 }
