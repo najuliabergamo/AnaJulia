61cbe5b7ff48b7220f0c0c16ed1556e624b4f471
Ana Julia <ana.j2007@aluno.ifsc.edu>
2025-12-01 10:52:02 -0300

    listagem nao planetas

--- app/src/main/java/com/example/mariaclara/MainActivity.java
+++ app/src/main/java/com/example/mariaclara/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.mariaclara;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Variáveis principais
+    String[] nomes = new String[] {
+            "MAGALI", "MONICA", "CEBOLINHA",
+            "CASCÃO", "CHICO BENTO"
+    };
+
+    ListView lv; // ListView onde os nomes vão aparecer
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_main); // Carrega o layout da tela principal
+
+        // Recupera a ListView do layout XML e liga a variável lv
+        lv = findViewById(R.id.listview);
+
+        // Criamos o Adapter que vai conectar os nomes ao layout de cada item da lista
+        ArrayAdapter<String> a = new ArrayAdapter(
+                this,
+                R.layout.item_lista, // Layout do item individual da lista
+                R.id.textView, // TextView dentro do layout que vai receber cada nome
+                nomes // Vetor de nomes que será usado como dados
+        );
+
+        // Define o adapter para exibir os nomes na ListView
+        lv.setAdapter(a);
     }
 }
